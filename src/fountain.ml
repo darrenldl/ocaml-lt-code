9edb93a873e78d46fc4b8a5efb53e31086aab8c9
Darren Ldl <darrenldldev@gmail.com>
2021-06-13 19:18:45 +1000

    Progress

--- src/fountain.ml
+++ src/fountain.ml
@@ -25,7 +25,7 @@ module Encode = struct
     let data_block_count_int = Ctx.data_block_count ctx in
     let drop_count = Ctx.drop_count ctx in
     let degrees = Array.make 0 drop_count in
-    (* fix first drop to be degree 1 to ensure decoding is possible *)
+    (* fix first drop to be degree 1 to ensure decoding is at least possible *)
     degrees.(0) <- 1;
     Random.self_init ();
     for i = 1 to drop_count - 1 do
@@ -76,6 +76,7 @@ module Decode = struct
     [ `Inconsistent_drop_size
     | `Invalid_drop_count
     | `Insufficient_drops
+    | `Invalid_data_block_buffer
     ]
 
   module Graph = struct
@@ -84,32 +85,104 @@ module Decode = struct
     type t = {
       data_len : int;
       data_blocks : bytes array;
-      data_block_solved : bool array;
-      drops : Drop.t array;
+      mutable unsolved_data_blocks : Int_set.t;
+      drops : Drop.t option array;
       data_edges : bucket array;
       drop_edges : bucket array;
     }
 
-    let propagate_xor ~data_index (g : t) : unit =
-      assert (g.data_block_solved.(data_index));
-      
+    let make_unsolved_data_blocks ~data_block_count : Int_set.t =
+      let rec aux c (acc : Int_set.t) =
+        if c < data_block_count then aux (succ c) (Int_set.add c acc) else acc
+      in
+      aux 0 Int_set.empty
 
-    let add_drop (g : t) ctx (drop : Drop.t) : unit =
-      let drop_index = Drop.index drop in
-      let data_block_indices = get_data_block_indices ctx drop in
-      List.iter
-        (fun data_index ->
-          g.data_edges.(data_index) <-
-            Int_set.add drop_index g.data_edges.(data_index))
-        data_block_indices;
-      g.drop_edges.(drop_index) <- Int_set.of_list data_block_indices;
-      if Drop.degree drop = 1 then
-        let data_index = List.hd data_block_indices in
-        Bytes.blit (Drop.data drop) 0
-          g.data_blocks.(data_index)
-          0 g.data_len;
-          g.data_block_solved.(data_index) <- true
-      else ()
+    let make ?(data_block_buffer : bytes array option) (ctx : Ctx.t)
+        (available_drops : Drop_set.t) : (t, error) result =
+      if Drop_set.cardinal available_drops <> Ctx.drop_count ctx then
+        Error `Invalid_drop_count
+      else
+        let drop_size =
+          Bytes.length (Drop.data @@ Drop_set.choose available_drops)
+        in
+        if
+          not
+            (Drop_set.for_all
+               (fun x -> Bytes.length (Drop.data x) = drop_size)
+               available_drops)
+        then Error `Inconsistent_drop_size
+        else
+          let data_blocks =
+            match data_block_buffer with
+            | None ->
+                Ok
+                  (Array.init (Ctx.data_block_count ctx) (fun _ ->
+                       Bytes.make drop_size '\x00'))
+            | Some buffer ->
+                if
+                  Array.length buffer = Ctx.data_block_count ctx
+                  && Array.for_all (fun b -> Bytes.length b = drop_size) buffer
+                then Ok buffer
+                else Error `Invalid_data_block_buffer
+          in
+          match data_blocks with
+          | Error e -> Error e
+          | Ok data_blocks ->
+              let data_block_count = Ctx.data_block_count ctx in
+              let drop_count = Ctx.drop_count ctx in
+              let drops = Array.make drop_count None in
+              let data_edges = Array.make data_block_count Int_set.empty in
+              let drop_edges = Array.make drop_count Int_set.empty in
+              Drop_set.iter
+                (fun drop ->
+                  let drop_index = Drop.index drop in
+                  drops.(drop_index) <- Some drop;
+                  let data_indices =
+                    Int_set.of_list @@ get_data_block_indices ctx drop
+                  in
+                  drop_edges.(drop_index) <- data_indices;
+                  Int_set.iter
+                    (fun data_index ->
+                      data_edges.(data_index) <-
+                        Int_set.add drop_index data_edges.(data_index))
+                    data_indices)
+                available_drops;
+              Ok
+                {
+                  data_len = drop_size;
+                  data_blocks;
+                  unsolved_data_blocks =
+                    make_unsolved_data_blocks ~data_block_count;
+                  drops;
+                  data_edges;
+                  drop_edges;
+                }
+
+    let propagate_data_xor ~data_index (g : t) : unit =
+      let data = g.data_blocks.(data_index) in
+      Int_set.iter
+        (fun drop_index ->
+          Utils.xor_onto ~src:data
+            ~onto:(Drop.data @@ Option.get g.drops.(drop_index)))
+        g.data_edges.(data_index)
+
+    let kick_start_with_degree_1_drops (g : t) : error option =
+      let degree_1_found = ref false in
+      Array.iter
+        (fun drop ->
+          match drop with
+          | None -> ()
+          | Some drop ->
+              if Drop.degree drop = 1 then (
+                degree_1_found := true;
+                let drop_index = Drop.index drop in
+                let data_index = Int_set.choose g.drop_edges.(drop_index) in
+                Bytes.blit (Drop.data drop) 0 g.data_blocks.(data_index) 0
+                  g.data_len;
+                g.unsolved_data_blocks <-
+                  Int_set.remove data_index g.unsolved_data_blocks))
+        g.drops;
+      if !degree_1_found then None else Some `Insufficient_drops
   end
 
   let decode (ctx : Ctx.t) (drops : Drop_set.t) : (bytes array, error) result =
