0df52afcbe3283f81eadcd5d2e0085146de9cf3e
Darren Ldl <darrenldldev@gmail.com>
2021-06-16 04:18:56 +1000

    Adjusting simulation output

--- sims/main.ml
+++ sims/main.ml
@@ -150,6 +150,7 @@ let () =
     "data block count: %d, drop count limit: %d (%3.3f%% redundancy), data \
      block size: %d\n"
     data_block_count drop_count_limit redundancy setup.data_block_size;
+  Printf.printf "data loss rate: %3.3f%%\n" (100.0 *. setup.data_loss_rate);
   Printf.printf
     "success rate: %3.3f%%, avg. overhead for successful cases: %3.3f%%\n"
     (100.0 *. stats.success_rate)
