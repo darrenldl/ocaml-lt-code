070a5a4d8a44a227ef0203b1a93ff05ac4fb2c26
Darren Ldl <darrenldldev@gmail.com>
2021-06-16 04:04:21 +1000

    Updated api use

--- sims/main.ml
+++ sims/main.ml
@@ -3,6 +3,8 @@ type setup = {
   data_block_size : int;
   data_loss_rate : float;
   rounds : int;
+  encoder : Ofountain.encoder;
+  decoder : Ofountain.decoder;
 }
 
 type stats = {
@@ -26,84 +28,94 @@ let empty_stats_sum = { total_overhead = 0.0; total_success_count = 0 }
 
 let make_setup ~systematic ~data_block_count ~max_redundancy ~data_block_size
     ~data_loss_rate ~rounds =
-  let drop_count =
+  let drop_count_limit =
     data_block_count
     + int_of_float (max_redundancy *. float_of_int data_block_count)
   in
   let param =
     Result.get_ok
-    @@ Ofountain.Param.make ~systematic ~data_block_count ~drop_count
+    @@ Ofountain.Param.make ~systematic ~data_block_count ~drop_count_limit
   in
   assert (0.0 <= data_loss_rate);
-  { param; data_block_size; data_loss_rate; rounds }
+  let drop_data_buffer =
+    Array.init (Ofountain.Param.drop_count_limit param) (fun _ ->
+        Cstruct.create data_block_size)
+  in
+  let data_blocks =
+    Array.init (Ofountain.Param.data_block_count param) (fun _ ->
+        Cstruct.create data_block_size)
+  in
+  let encoder =
+    Result.get_ok @@ Ofountain.make_encoder ~drop_data_buffer
+    param
+    data_blocks
+  in
+  let data_block_buffer =
+    Array.init (Ofountain.Param.data_block_count param) (fun _ ->
+        Cstruct.create data_block_size)
+  in
+  let decoder =
+    Result.get_ok
+    @@ Ofountain.make_decoder ~data_block_buffer
+         ~data_block_size:data_block_size param
+  in
+  { param; data_block_size; data_loss_rate; rounds; encoder; decoder }
 
-let run_once ~data_block_buffer ~drop_data_buffer (setup : setup) data_blocks :
+let run_once (setup : setup) :
     stats =
-  let rec aux (original_data_blocks : Cstruct.t array)
-      (decode_ctx : Ofountain.decode_ctx) (stats : stats)
-      (drops : unit -> Ofountain.drop option) : stats =
-    match drops () with
+  let rec aux (data_blocks_copy : Cstruct.t array)
+      (stats : stats)
+      : stats =
+    match Ofountain.encode_one_drop setup.encoder with
     | None -> stats
     | Some x -> (
         Random.self_init ();
         if Random.float 1.0 < setup.data_loss_rate then
-          aux original_data_blocks decode_ctx stats drops
+          aux data_blocks_copy stats
         else
           let stats = { stats with drops_used = stats.drops_used + 1 } in
-          match Ofountain.decode_drop decode_ctx x with
+          match Ofountain.decode_one_drop setup.decoder x with
           | Ok (`Success arr) ->
               for
                 i = 0
-                to Ofountain.Param.data_block_count
-                     (Ofountain.param_of_decode_ctx decode_ctx)
+                to Ofountain.data_block_count_of_decoder setup.decoder
                    - 1
               do
-                let same = Cstruct.equal arr.(i) original_data_blocks.(i) in
+                let same = Cstruct.equal arr.(i) data_blocks_copy.(i) in
                 if not same then (
                   Fmt.pr "recovered: %a\n" Cstruct.hexdump_pp arr.(i);
                   Fmt.pr "original:  %a\n" Cstruct.hexdump_pp
-                    original_data_blocks.(i));
+                    data_blocks_copy.(i));
                 assert same
               done;
               { stats with success = true }
-          | Ok `Ongoing -> aux original_data_blocks decode_ctx stats drops
+          | Ok `Ongoing -> aux data_blocks_copy stats
           | Error `Cannot_recover -> stats
           | Error _ -> failwith "Unexpected case")
   in
-  let drops =
-    Result.get_ok
-    @@ Ofountain.encode_with_param_lazy ~drop_data_buffer setup.param
-         data_blocks
-  in
-  let decode_ctx =
-    Result.get_ok
-    @@ Ofountain.make_decode_ctx ~data_block_buffer
-         ~data_block_size:setup.data_block_size setup.param
-  in
-  aux data_blocks decode_ctx empty_stats drops
-
-let run (setup : setup) : combined_stats =
-  let data_blocks =
-    Array.init (Ofountain.Param.data_block_count setup.param) (fun _ ->
-        Cstruct.create setup.data_block_size)
-  in
+  Ofountain.reset_encoder setup.encoder;
+  Ofountain.reset_decoder setup.decoder;
+  let data_block_count = Ofountain.data_block_count_of_encoder setup.encoder in
+  let data_block_size = Ofountain.data_block_size_of_encoder setup.encoder in
+  let data_blocks = Ofountain.data_blocks_of_encoder setup.encoder in
   Array.iter
     (fun block ->
       for i = 0 to setup.data_block_size - 1 do
         Cstruct.set_uint8 block i (Random.int 256)
       done)
     data_blocks;
-  let data_block_buffer =
-    Array.init (Ofountain.Param.data_block_count setup.param) (fun _ ->
-        Cstruct.create setup.data_block_size)
+  let data_blocks_copy = Array.init data_block_count (fun i ->
+    let x = Cstruct.create data_block_size in
+    Cstruct.blit data_blocks.(i) 0 x 0 data_block_size;
+    x
+    )
   in
-  let drop_data_buffer =
-    Array.init (Ofountain.Param.drop_count setup.param) (fun _ ->
-        Cstruct.create setup.data_block_size)
-  in
+  aux data_blocks_copy empty_stats
+
+let run (setup : setup) : combined_stats =
   let stats_collection =
     Array.init setup.rounds (fun _ ->
-        run_once ~data_block_buffer ~drop_data_buffer setup data_blocks)
+        run_once setup)
   in
   let data_block_count =
     float_of_int @@ Ofountain.Param.data_block_count setup.param
