9d2b525d2190401a71df2946ac4041b398428d28
Darren Ldl <darrenldldev@gmail.com>
2021-06-18 16:18:52 +1000

    Fixing hash_int_set

--- src/hash_int_set.ml
+++ src/hash_int_set.ml
@@ -1,61 +1,64 @@
-type t = Int_set.t array
+type t = {
+  seed : int;
+  buckets : Int_set.t array;
+}
 
 let hash (t : t) (x : int) =
   let r =
-  abs (
-  (Rand.gen_int_global max_int)
-  lxor x
-)
+  abs (t.seed lxor x)
   land
   max_int
   in
   assert (r >= 0);
-  r mod Array.length t
+  r mod Array.length t.buckets
 
-let create size =
-  Array.make size Int_set.empty
+let create size : t =
+  {
+    seed = Rand.gen_int_global max_int;
+  buckets = Array.make size Int_set.empty;
+}
 
 let add (t : t) (x : int) : unit =
   let i = hash t x in
-  t.(i) <-
-    Int_set.add x t.(i)
+  t.buckets.(i) <-
+    Int_set.add x t.buckets.(i)
 
 let mem (t : t) (x : int) : bool =
   let i = hash t x in
-  Int_set.mem x t.(i)
+  Int_set.mem x t.buckets.(i)
 
 let remove (t : t) x =
   let i = hash t x in
-  t.(i) <-
-    Int_set.remove x t.(i)
+  t.buckets.(i) <-
+    Int_set.remove x t.buckets.(i)
 
-let cardinal t =
+let cardinal (t : t) =
   Array.fold_left (fun c s ->
     c + Int_set.cardinal s
     )
   0
-  t
+  t.buckets
 
-let iter (f : int -> unit) t : unit =
+let iter (f : int -> unit) (t : t) : unit =
   Array.iter (fun s ->
     Int_set.iter f s
-) t
+) t.buckets
 
-let reset t =
-  for i=0 to Array.length t-1 do
-    t.(i) <- Int_set.empty
+let reset (t : t) =
+  for i=0 to Array.length t.buckets -1 do
+    t.buckets.(i) <- Int_set.empty
   done
 
 let choose_opt t =
-  let rec aux cur len t =
+  let rec aux cur len buckets =
     if cur < len then
-      match Int_set.choose_opt t.(cur) with
-    | None -> aux (succ cur) len t
+      match Int_set.choose_opt buckets.(cur) with
+    | None -> aux (succ cur) len buckets
     | Some x -> Some x
     else
       None
   in
-  aux 0 (Array.length t) t
+  aux 0 (Array.length t.buckets) t.buckets
 
 let choose t =
   Option.get (choose_opt t)
