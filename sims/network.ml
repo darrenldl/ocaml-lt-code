7f3ee78e304ffa3802b82a3dd4f1dd68e1283e28
Darren Ldl <darrenldldev@gmail.com>
2021-06-18 20:37:47 +1000

    Tuning params

--- sims/network.ml
+++ sims/network.ml
@@ -6,8 +6,8 @@ let rounds = 100
 
 let setups =
   [
-    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:1000
-      ~max_redundancy:0.30 ~data_block_size:490 ~data_loss_rate:0.30 ~rounds;
+    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:1000
+      ~max_redundancy:0.30 ~data_block_size:490 ~data_loss_rate:0.05 ~rounds;
   ]
 
 let () =
--- sims/storage.ml
+++ sims/storage.ml
@@ -7,7 +7,7 @@ let rounds = 100
 let setups =
   [
     make_setup ~systematic:true ~encode_all_upfront ~data_block_count:1000
-      ~max_redundancy:0.30 ~data_block_size:490 ~data_loss_rate:0.30 ~rounds;
+      ~max_redundancy:0.30 ~data_block_size:490 ~data_loss_rate:0.01 ~rounds;
   ]
 
 let () =
