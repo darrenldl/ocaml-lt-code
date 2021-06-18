6f2d5fdb3d24053072ef50d356321bac8367ae2e
Darren Ldl <darrenldldev@gmail.com>
2021-06-18 16:31:47 +1000

    Swapped to using hashtbl with custom hash for hash_int_set

--- src/hash_int_set.ml
+++ src/hash_int_set.ml
@@ -1,64 +1,43 @@
-type t = {
-  seed : int;
-  buckets : Int_set.t array;
-}
+let seed = Rand.gen_int_global max_int
 
-let hash (t : t) (x : int) =
+let hash (x : int) =
   let r =
-  abs (t.seed lxor x)
+  abs (seed lxor x)
   land
   max_int
   in
   assert (r >= 0);
-  r mod Array.length t.buckets
+  r
+
+module H = Hashtbl.Make (struct type t = int let equal = Int.equal let hash = hash end)
+
+type t = unit H.t
 
 let create size : t =
-  {
-    seed = Rand.gen_int_global max_int;
-  buckets = Array.make size Int_set.empty;
-}
+  H.create size
 
 let add (t : t) (x : int) : unit =
-  let i = hash t x in
-  t.buckets.(i) <-
-    Int_set.add x t.buckets.(i)
+  H.add t x ()
 
 let mem (t : t) (x : int) : bool =
-  let i = hash t x in
-  Int_set.mem x t.buckets.(i)
+  H.mem t x
 
 let remove (t : t) x =
-  let i = hash t x in
-  t.buckets.(i) <-
-    Int_set.remove x t.buckets.(i)
+  H.remove t x
 
 let cardinal (t : t) =
-  Array.fold_left (fun c s ->
-    c + Int_set.cardinal s
-    )
-  0
-  t.buckets
+  H.length t
 
 let iter (f : int -> unit) (t : t) : unit =
-  Array.iter (fun s ->
-    Int_set.iter f s
-) t.buckets
+  H.iter (fun x () -> f x) t
 
 let reset (t : t) =
-  for i=0 to Array.length t.buckets -1 do
-    t.buckets.(i) <- Int_set.empty
-  done
+  H.reset t
 
 let choose_opt t =
-  let rec aux cur len buckets =
-    if cur < len then
-      match Int_set.choose_opt buckets.(cur) with
-    | None -> aux (succ cur) len buckets
-    | Some x -> Some x
-    else
-      None
-  in
-  aux 0 (Array.length t.buckets) t.buckets
+  match H.to_seq_keys t () with
+  | Seq.Nil -> None
+  | Seq.Cons (x, _) -> Some x
 
 let choose t =
   Option.get (choose_opt t)
