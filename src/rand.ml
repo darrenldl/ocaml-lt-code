f22e95a8ab169b86a613c29409305ff7446d4564
Darren Ldl <darrenldldev@gmail.com>
2021-06-18 15:09:09 +1000

    Swapped to lehmer generator

--- src/rand.ml
+++ src/rand.ml
@@ -1,22 +1,21 @@
-type state = int64 ref
+type state = int32 ref
 
 let make seed : state =
-  let seed = Int64.of_int seed in
+  let seed = Int32.of_int seed in
   ref
-    (if Int64.equal seed 0L then Int64.succ 0L
-    else if Int64.equal seed Int64.max_int then Int64.(pred max_int)
+    (if Int32.equal seed 0l then Int32.succ 0l
+    else if Int32.equal seed Int32.max_int then Int32.(pred max_int)
     else seed)
 
-let gen_int64 (state : state) (bound : int64) : int64 =
-  let x = !state in
-  let x = Int64.logxor x (Int64.shift_left x 13) in
-  let x = Int64.logxor x (Int64.shift_right_logical x 7) in
-  let x = Int64.logxor x (Int64.shift_left x 17) in
-  assert (x <> 0L);
+let gen_int32 (state : state) (bound : int32) : int32 =
+  assert (bound > 0l);
+  let x = Int64.(logand 0xFFFF_FFFFL (of_int32 !state)) in
+  let x = Int64.(rem (mul x 48271L) 0x7FFF_FFFFL)in
+  let x = Int64.to_int32 x in
+  assert (x <> 0l);
   state := x;
-  let x = if x = Int64.max_int then Int64.(pred max_int) else x in
-  let x = Int64.logand x 0x7FFFFFFF_FFFFFFFFL in
-  let r = Int64.rem x bound in
+  let x = Int32.logand x 0x7FFF_FFFFl in
+  let r = Int32.rem x bound in
   r
 
 let global =
@@ -24,10 +23,11 @@ let global =
   let seed = Random.int 0x0FFFFFFF in
   make seed
 
-let gen_int64_global bound = gen_int64 global bound
+let gen_int32_global bound = gen_int32 global bound
 
 let gen_int (state : state) (bound : int) : int =
-  let r = Int64.to_int @@ gen_int64 state (Int64.of_int bound) in
+  assert (bound <= Int32.(to_int max_int));
+  let r = Int32.to_int @@ gen_int32 state (Int32.of_int bound) in
   r
 
 let gen_int_global bound = gen_int global bound
--- src/rand.mli
+++ src/rand.mli
@@ -4,8 +4,8 @@ val make : int -> state
 
 val gen_int : state -> int -> int
 
-val gen_int64 : state -> int64 -> int64
+val gen_int32 : state -> int32 -> int32
 
 val gen_int_global : int -> int
 
-val gen_int64_global : int64 -> int64
+val gen_int32_global : int32 -> int32
