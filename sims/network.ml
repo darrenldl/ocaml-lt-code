d929cbe38ee201076dee3258f40663aa9a7e6fe6
Darren Ldl <darrenldldev@gmail.com>
2021-06-19 16:22:53 +1000

    Added more simulation scenarios

--- sims/network.ml
+++ sims/network.ml
@@ -8,9 +8,15 @@ let setups =
   [
     make_setup ~systematic:false ~encode_all_upfront ~data_block_count:1000
       ~max_redundancy:1.00 ~data_block_size:1300 ~data_loss_rate:0.05 ~rounds;
+    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:1000
+      ~max_redundancy:1.00 ~data_block_size:1300 ~data_loss_rate:0.05 ~rounds;
     make_setup ~systematic:false ~encode_all_upfront ~data_block_count:1000
       ~max_redundancy:1.00 ~data_block_size:1300 ~data_loss_rate:0.01 ~rounds;
+    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:1000
+      ~max_redundancy:1.00 ~data_block_size:1300 ~data_loss_rate:0.01 ~rounds;
     make_setup ~systematic:false ~encode_all_upfront ~data_block_count:1000
+      ~max_redundancy:1.00 ~data_block_size:1300 ~data_loss_rate:0.005 ~rounds;
+    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:1000
       ~max_redundancy:1.00 ~data_block_size:1300 ~data_loss_rate:0.005 ~rounds;
   ]
 
