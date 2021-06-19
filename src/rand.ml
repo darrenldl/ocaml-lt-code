c0bb400a31fc7759c7ba3cf9f824c3751fa34d56
Darren Ldl <darrenldldev@gmail.com>
2021-06-19 20:27:06 +1000

    Switched to Lehmer generator

--- src/rand.ml
+++ src/rand.ml
@@ -1,40 +1,40 @@
-type state = int64 ref
+type state = int32 ref
+
+let mask = 0x7FFFFFFFl
 
 let create seed : state =
-  let seed = Int64.of_int seed in
+  let seed = Int32.of_int seed in
   ref
-    (if Int64.equal seed 0L then Int64.succ 0L
-    else if Int64.equal seed Int64.max_int then Int64.(pred max_int)
-    else seed)
+    (if Int32.equal seed 0l then Int32.succ 0l
+    else Int32.logand seed mask
+    )
 
-let hash_int64 (x : int64) : int64 =
-  let x = Int64.logxor x (Int64.shift_left x 13) in
-  let x = Int64.logxor x (Int64.shift_right_logical x 7) in
-  Int64.logxor x (Int64.shift_left x 17)
+let hash_int32 (x : int32) : int32 =
+  let x = Int64.of_int32 x in
+  let x = Int64.(mul x 48271L) in 
+  let x = Int64.(rem x 0x7FFF_FFFFL) in
+  Int64.to_int32 x
 
-let hash_int (x : int) : int = Int64.to_int @@ hash_int64 (Int64.of_int x)
+let hash_int (x : int) : int = Int32.to_int @@ hash_int32 (Int32.of_int x)
 
-let gen_int64 (state : state) (bound : int64) : int64 =
-  let mask = 0x7FFFFFFF_FFFFFFFFL in
+let gen_int32 (state : state) (bound : int32) : int32 =
   let rec aux state bound retry_start =
-    let x = hash_int64 !state in
-    assert (x <> 0L);
+    let x = hash_int32 !state in
+    assert (x <> 0l);
     state := x;
     (* let x = if x = Int64.max_int then Int64.(pred max_int) else x in *)
-    let x = Int64.logand x mask in
-    if x < retry_start then Int64.rem x bound else aux state bound retry_start
+    let x = Int32.logand x mask in
+    if x < retry_start then Int32.rem x bound else aux state bound retry_start
   in
-  aux state bound Int64.(sub mask (rem mask bound))
+  aux state bound Int32.(sub mask (rem mask bound))
 
 let global =
   Random.self_init ();
   let seed = Random.int 0x0FFFFFFF in
   create seed
 
-let gen_int64_global bound = gen_int64 global bound
-
 let gen_int (state : state) (bound : int) : int =
-  let r = Int64.to_int @@ gen_int64 state (Int64.of_int bound) in
+  let r = Int32.to_int @@ gen_int32 state (Int32.of_int bound) in
   r
 
 let gen_int_global bound = gen_int global bound
--- src/rand.mli
+++ src/rand.mli
@@ -4,12 +4,6 @@ val create : int -> state
 
 val gen_int : state -> int -> int
 
-val gen_int64 : state -> int64 -> int64
-
 val gen_int_global : int -> int
 
-val gen_int64_global : int64 -> int64
-
 val hash_int : int -> int
-
-val hash_int64 : int64 -> int64
