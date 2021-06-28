cde343e6ebb2590ec876a6c16031391aa8bb87e2
Darren Ldl <darrenldldev@gmail.com>
2021-06-28 14:14:03 +1000

    Code refactoring

--- src/rand.ml
+++ src/rand.ml
@@ -4,10 +4,7 @@ let modulus = 0x7FFF_FFFFL
 
 let create_rng seed : rng =
   let seed = Int64.of_int seed in
-  {
-    state =
-      (if Int64.equal seed 0L then Int64.succ 0L else Int64.logand seed modulus);
-  }
+  { state = Int64.(logand (logor seed 1L) modulus) }
 
 let hash' (x : int64) : int64 =
   let x = Int64.(mul x 48271L) in
