5b982548454f0e454d89c60dd431bdf6013f4117
Darren Ldl <darrenldldev@gmail.com>
2021-06-29 01:18:36 +1000

    Testing simulation params

--- sims/storage.ml
+++ sims/storage.ml
@@ -6,6 +6,12 @@ let rounds = 200
 
 let setups =
   [
+    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:500
+      ~max_redundancy:1.00 ~data_block_size:490 ~data_loss_rate:0.30 ~rounds;
+    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:500
+      ~max_redundancy:1.00 ~data_block_size:490 ~data_loss_rate:0.20 ~rounds;
+    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:100
+      ~max_redundancy:0.50 ~data_block_size:490 ~data_loss_rate:0.20 ~rounds;
     make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100
       ~max_redundancy:0.20 ~data_block_size:490 ~data_loss_rate:0.01 ~rounds;
     make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100
