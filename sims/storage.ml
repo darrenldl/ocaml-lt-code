0e6ef8cf48430f539bed91c51969fd23d4127dad
Darren Ldl <darrenldldev@gmail.com>
2021-07-01 01:57:13 +1000

    Moved ofountain to lt_code

--- sims/storage.ml
+++ sims/storage.ml
@@ -6,20 +6,20 @@ let rounds = 200
 
 let setups =
   [
-    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:500
-      ~max_redundancy:1.00 ~data_block_size:490 ~data_loss_rate:0.30 ~rounds;
-    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:500
-      ~max_redundancy:1.00 ~data_block_size:490 ~data_loss_rate:0.20 ~rounds;
-    make_setup ~systematic:false ~encode_all_upfront ~data_block_count:100
-      ~max_redundancy:0.50 ~data_block_size:490 ~data_loss_rate:0.20 ~rounds;
     make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100
-      ~max_redundancy:0.20 ~data_block_size:490 ~data_loss_rate:0.01 ~rounds;
-    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100
-      ~max_redundancy:0.20 ~data_block_size:4000 ~data_loss_rate:0.02 ~rounds;
-    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100
-      ~max_redundancy:0.20 ~data_block_size:4000 ~data_loss_rate:0.01 ~rounds;
-    make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100
-      ~max_redundancy:0.20 ~data_block_size:4000 ~data_loss_rate:0.005 ~rounds;
+      ~max_redundancy:0.50 ~data_block_size:490 ~data_loss_rate:0.25 ~rounds;
+    (* make_setup ~systematic:true ~encode_all_upfront ~data_block_count:500 *)
+      (* ~max_redundancy:1.00 ~data_block_size:490 ~data_loss_rate:0.20 ~rounds; *)
+    (* make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100 *)
+      (* ~max_redundancy:0.50 ~data_block_size:490 ~data_loss_rate:0.20 ~rounds; *)
+    (* make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100 *)
+      (* ~max_redundancy:0.20 ~data_block_size:490 ~data_loss_rate:0.01 ~rounds; *)
+    (* make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100 *)
+      (* ~max_redundancy:0.20 ~data_block_size:4000 ~data_loss_rate:0.02 ~rounds; *)
+    (* make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100 *)
+      (* ~max_redundancy:0.20 ~data_block_size:4000 ~data_loss_rate:0.01 ~rounds; *)
+    (* make_setup ~systematic:true ~encode_all_upfront ~data_block_count:100 *)
+      (* ~max_redundancy:0.20 ~data_block_size:4000 ~data_loss_rate:0.005 ~rounds; *)
   ]
 
 let () =
