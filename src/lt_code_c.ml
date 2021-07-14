97ae111bb016aa45ba9f7d47816affa9692160e8
Darren Ldl <darrenldldev@gmail.com>
2021-07-15 00:14:36 +1000

    Formatting

--- src/lt_code_c.ml
+++ src/lt_code_c.ml
@@ -2,9 +2,6 @@ open Ctypes
 open Foreign
 
 let xor_onto =
-  foreign "xor_onto"
-  (int @-> ptr char @-> ptr char @-> returning void)
+  foreign "xor_onto" (int @-> ptr char @-> ptr char @-> returning void)
 
-let memcpy =
-  foreign "memcpy"
-  (ptr char @-> ptr char @-> int @-> returning void)
+let memcpy = foreign "memcpy" (ptr char @-> ptr char @-> int @-> returning void)
