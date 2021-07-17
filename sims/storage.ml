95deaa80b7bd2dd9223bb7c9fa557af0d8eb1649
Darren Ldl <darrenldldev@gmail.com>
2021-07-18 02:44:30 +1000

    Tuning storage sim

--- sims/storage.ml
+++ sims/storage.ml
@@ -4,6 +4,8 @@ let encode_all_upfront = true
 
 let rounds = 100
 
+let data_block_size = 4 * 1024
+
 let setups =
   [
     (* make_setup ~systematic:true ~systematic_scaling_factor:2.0 *)
@@ -11,28 +13,28 @@ let setups =
     (* ~data_block_size:4000 ~data_loss_rate:0.30 ~rounds (); *)
     make_setup ~systematic:true ~systematic_scaling_factor:2.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.50
-      ~data_block_size:4000 ~data_loss_rate:0.25 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.25 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:2.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.50
-      ~data_block_size:4000 ~data_loss_rate:0.20 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.20 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:2.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.50
-      ~data_block_size:4000 ~data_loss_rate:0.20 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.20 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:2.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.30
-      ~data_block_size:4000 ~data_loss_rate:0.15 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.15 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:10.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.20
-      ~data_block_size:4000 ~data_loss_rate:0.01 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.01 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:10.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.20
-      ~data_block_size:4000 ~data_loss_rate:0.02 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.02 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:10.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.20
-      ~data_block_size:4000 ~data_loss_rate:0.01 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.01 ~rounds ();
     make_setup ~systematic:true ~systematic_scaling_factor:20.0
       ~encode_all_upfront ~data_block_count:1000 ~max_redundancy:0.20
-      ~data_block_size:4000 ~data_loss_rate:0.005 ~rounds ();
+      ~data_block_size ~data_loss_rate:0.005 ~rounds ();
   ]
 
 let () =
